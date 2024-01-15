438516f4f6540d4c0b52108a258f39f17a718867
navya <navya@metrictreelabs.com>
2024-01-15 23:03:10 +0530

    updation:class functions updated definition added for calculator class funtions

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ class Calculator {
   constructor(prevOpTextEle, currentOpTextEle) {
     (this.currentOpTextEle = currentOpTextEle),
       (this.prevOpTextEle = prevOpTextEle);
+    this.clear();
   }
-  clear() {}
+  clear() {
+    this.currentOpTextEle = "";
+    this.prevOpTextEle = "";
+    this.operation = undefined;
+  }
   delete() {}
   append(num) {}
 
@@ -11,7 +16,9 @@ class Calculator {
 
   calculate() {}
 
-  displayResult() {}
+  displayResult() {
+    this.currentOpTextEle.innerText = this.currentOpTextEle;
+  }
 }
 const numberButton = document.querySelectorAll("[data-num]");
 const operandButton = document.querySelectorAll("[data-op]");
@@ -20,3 +27,11 @@ const delButton = document.querySelector("[data-del]");
 const clearButton = document.querySelector("[data-clr-all]");
 const prevOpTextEle = document.querySelector("[data-previous-operand]");
 const currentOpTextEle = document.querySelector("[data-current-operand]");
+
+const calculator = new Calculator(prevOpTextEle, currentOpTextEle);
+numberButton.forEach((key) => {
+  key.addEventListener("click", () => {
+    calculator.append(key.innerText);
+    calculator.displayResult();
+  });
+});
